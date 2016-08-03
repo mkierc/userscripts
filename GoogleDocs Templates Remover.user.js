bfc6668fae92c8a219728bf3725f0694300318f4
mkierc <urban.pl@gmail.com>
2016-08-03 18:30:38 +0200

    Fixed after Google changed the styling

--- GoogleDocs Templates Remover.user.js
+++ GoogleDocs Templates Remover.user.js
@@ -1,12 +1,12 @@
 /**
  * GoogleDocs Templates Remover
  * Userscript for removing the ugly "Start a new document" banner from Google Docs / Sheets / Slides
- * Copyright © 2015 by M. Kierc. All rights reserved.
- * 
+ * Copyright © 2016 by M. Kierc. All rights reserved.
+ *
  * GoogleDocs Templates Remover is licensed under the GNU General Public License v3.
  * You can redistribute it and/or modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation, either version 3 of the License, or any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details. <http://www.gnu.org/licenses/gpl.html>.
@@ -29,9 +29,10 @@
 (
     function() {
         var css = [
-        ".docs-homescreen-template-wrapper {        ",
-        "   display: none;                          ",
-        "}                                          ",
+        ".docs-homescreen-docs .docs-homescreen-fcc-inlinecontainer-pinned-contracted, .docs-homescreen-docs .docs-homescreen-fcc-flex-contracting, .docs-homescreen-docs .docs-homescreen-fcc-flex-contracted, .docs-homescreen-docs .docs-homescreen-fcc-flex-unhiding {",
+        "   min-height: 50px;                                                                                                                                                                                                                                             ",
+        "   height: 50px;                                                                                                                                                                                                                                                 ",
+        "}                                                                                                                                                                                                                                                                ",
         ].join("\n");
         if (typeof GM_addStyle != "undefined") {
             GM_addStyle(css);
@@ -45,7 +46,7 @@
             node.appendChild(document.createTextNode(css));
             var heads = document.getElementsByTagName("head");
             if (heads.length > 0) {
-                heads[0].appendChild(node); 
+                heads[0].appendChild(node);
             } else {
                 document.documentElement.appendChild(node);
             }
